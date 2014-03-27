884da1c4133b1b797354662926c77b7229bda27d
Hakimu Jackson <hakimu@newrelic.com>
2014-03-26 20:20:49 -0700

    Added line in config/application.rb to deploy to Heroku

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #adding from Heroku help page
+    config.assets.initialize_on_precompile = false
   end
 end
